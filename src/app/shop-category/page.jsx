8cc6787474cb5267e8055467844b4635d6653b91
Madhu Pandey <madhupandey29052004@gmail.com>
2026-03-05 11:33:34 +0530

    career and category page

--- src/app/shop-category/page.jsx
+++ src/app/shop-category/page.jsx
@@ -2,7 +2,7 @@ import Wrapper from "@/layout/wrapper";
 import HeaderTwo from "@/layout/headers/header-2";
 import Footer from "@/layout/footers/footer";
 import ShopBreadcrumb from "@/components/breadcrumb/shop-breadcrumb";
-import ShopCategoryArea from "@/components/categories/shop-category-area";
+import CategoryShowcase from "@/components/category/CategoryShowcase";
 
 export const metadata = {
   title: "Shofy - Category Page",
@@ -13,7 +13,7 @@ export default function CategoryPage() {
     <Wrapper>
       <HeaderTwo style_2={true} />
       <ShopBreadcrumb title="Only Categories" subtitle="Only Categories" />
-      <ShopCategoryArea />
+      <CategoryShowcase />
       <Footer primary_style={true} />
     </Wrapper>
   );
